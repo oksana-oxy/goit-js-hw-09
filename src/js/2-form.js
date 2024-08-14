72af8f42599f9ac99815a4b6f6700ab3b121c542
oksana-oxy <pivenoxana143@gmail.com>
2024-08-14 19:42:37 +0300

    task finished

---
+++ src/js/2-form.js
@@ -0,0 +1,41 @@
+// const { normalizePath } = require("vite");
+
+const feedbackFormEl = document.querySelector('.js-feedback-form');
+let formData = {};
+
+const fillFormFields = () => {
+    const formDataFromLS = JSON.parse(localStorage.getItem('feedback-form-state'));
+    if (formDataFromLS === null) {
+        return;
+    }
+
+    formData = formDataFromLS;
+    for (const key in formDataFromLS) {
+        if (formDataFromLS.hasOwnProperty(key)) {
+            feedbackFormEl.elements[key].value = formDataFromLS[key];
+        }
+    }
+ };
+fillFormFields();
+
+const onFormFieldInput = event => {
+    const fildName = event.target.name;
+    const fieldValue = event.target.value.trim();
+    
+    formData[fildName] = fieldValue;
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+};
+
+const onFeedbackFormSubmit = event => {
+    event.preventDefault();
+    if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+    }
+    console.log(formData);
+    event.target.reset();
+    localStorage.removeItem('feedback-form-state');
+}
+
+feedbackFormEl.addEventListener('input', onFormFieldInput);
+feedbackFormEl.addEventListener('submit', onFeedbackFormSubmit);
